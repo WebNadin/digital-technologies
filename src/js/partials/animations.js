e37994008e960b62f573a5d0e7186d51dda16dde
WebNadin <nenadinweb@gmail.com>
2020-07-23 03:42:06 +0300

    init images move-animations were added

--- src/js/partials/animations.js
+++ src/js/partials/animations.js
@@ -1,18 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('mousemove', (e) => {
-    parallaxIt(e, ".js-parallaxCard", -60);
-    parallaxIt(e, ".js-parallaxHand", -40);
-    parallaxIt(e, ".js-parallaxCoin", -20);
-  });
+class Parallax {
 
-});
+  init() {
+    this.animationYoyo();
+    window.addEventListener('mousemove', (e) => {
+      this.mouseMoveElement(e, ".js-parallaxCard", -60);
+      this.mouseMoveElement(e, ".js-parallaxHand", -40);
+      this.mouseMoveElement(e, ".js-parallaxCoin", -20);
+    });
+    window.addEventListener('mouseover', (e) => {
+      gsap.killTweensOf(".js-parallaxCard");
+      gsap.killTweensOf(".js-parallaxCoin");
+      gsap.killTweensOf(".js-parallaxHand");
+    });
+    window.addEventListener('mouseout', (e) => {
+      this.animationYoyo();
+    });
+  }
 
-function parallaxIt(e, target, movement) {
-  let container = document.querySelector('.js-parallaxCard');
-  let relX = e.pageX - container.offsetLeft;
-  let relY = e.pageY - container.offsetTop;
-  gsap.to(target, 1, {
-    x: (relX - container.offsetWidth / 2) / container.offsetWidth * movement,
-    y: (relY - container.offsetHeight / 2) / container.offsetHeight * movement
-  })
+  animationYoyo() {
+    gsap.to(".js-parallaxCard", {y: 20, x: 20, duration: 2, repeat: 10, yoyoEase: true});
+    gsap.to(".js-parallaxCoin", {y: -10, x: -10, delay: .3, duration: 1, repeat: 10, yoyoEase: true});
+    gsap.to(".js-parallaxHand", {y: 10, x: 30, delay: .1, duration: 3, repeat: 10, yoyoEase: true});
+  }
+
+  mouseMoveElement(e, target, movement) {
+    let container = document.querySelector('.js-parallaxContainer');
+    let relX = e.pageX - container.offsetLeft;
+    let relY = e.pageY - container.offsetTop;
+    gsap.to(target, 1, {
+      x: (relX - container.offsetWidth / 2) / container.offsetWidth * movement,
+      y: (relY - container.offsetHeight / 2) / container.offsetHeight * movement
+    })
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  let parallaxImg = new Parallax(document.querySelector('.js-parallaxContainer'));
+  parallaxImg.init();
+});
